910748ecb2e8bd7a00918fe71134c7f0e7947477
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-21 18:12:41 +0200

    excercice 1
    teste eins ausgabe 1x

---
+++ Udmy-Java-Kurs-Grundlagen/src/Aufgabe1.java
@@ -0,0 +1,34 @@
+
+public class Aufgabe1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Aufgabe:
+		 * 
+		 *  1. Auto:           3500
+		 *  2. Alter:          18 
+		 *  3. Vorname:        Peter
+		 *  4. Nachname:        Müller
+		 *  
+		 *  
+		 *  1. Überlege Dir welchen Datentyp diese informationen repräsentieren.
+		 *  2. Lege Variablen fest und speicher diese Werte in den Variablen ab
+		 *  3. Gebe die Werte mit Hilfe von System.ou.printlm("") aus.
+		 */
+		
+		
+		int autoKosten1 = 3500; 
+		int alter1 = 18;
+		String vorname =    " Peter ";
+		String Nachname =   " Müller ";
+		
+		
+		System.out.println("Sein Vorname   ist: " + vorname );
+		System.out.println("Sein Nachname  ist: " + Nachname );
+		System.out.println(" Er hat ein Alter von: " + alter1 );
+		System.out.println("Sein Auto hat einen Wert von: " + autoKosten1 + " Euro " );
+	}
+
+}
