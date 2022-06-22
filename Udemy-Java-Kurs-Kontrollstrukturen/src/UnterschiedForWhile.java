c57a22b88b86d18f6b8cab2e4b99ee4ea3eea1f5
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-22 17:43:26 +0200

    difference between for and while

---
+++ Udemy-Java-Kurs-Kontrollstrukturen/src/UnterschiedForWhile.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class UnterschiedForWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 
+		 * for (Anfangswerte; Abbruchbedinungen; Zähler) {
+		 *       }
+		 * 
+		 * 
+		 * 
+		 * while(Bedinungen)
+		 *    { 
+		 *   	Anweisungsblock
+		 * 	  }                     Merke: Bedinung kann entweder true = wahr oder false = nicht wahr sein = while
+		 *  
+		 */
+		
+		 // wenn man genau weiss wie oft man die schleife durchlaufen lassen muss
+//		for(int i = 1; i<=5; i++) {                   
+//			System.out.println("Asteroid wurde erstellt Nr: " + i);
+//		}
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Drücke 1 für Schwierigkeitsgrad leicht \n Drücke 2 für Schwierigkeitsgrad Mittel");
+		
+		// wenn man nicht weiss wie oft man die schleife braucht 
+		int userEingabe = scan.nextInt();
+		int anzahlGegner = 0;
+		
+		if (userEingabe == 1) {
+			anzahlGegner = 10;
+		} else if (userEingabe == 2) {
+			anzahlGegner = 15;
+		}
+		
+		while(anzahlGegner != 0) {
+			System.out.println(" Gegner erstellt! ");
+			anzahlGegner --;
+				
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
