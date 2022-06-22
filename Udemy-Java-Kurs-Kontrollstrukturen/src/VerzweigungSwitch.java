ce21dd1a1fa501621e9476d13930c1574ee52f5e
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-22 15:58:18 +0200

    if switch how it works

---
+++ Udemy-Java-Kurs-Kontrollstrukturen/src/VerzweigungSwitch.java
@@ -0,0 +1,38 @@
+
+public class VerzweigungSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Kontrollstrukturen steuern den Programmablaufplan
+		
+		// switch = Fallunterscheidung 
+		// case = Der Fall
+		
+		// (Bedingung) als Bedingung gilt nur ein int, char und ein string
+		
+		
+		switch(5) 
+		{
+		case 0:
+			System.out.println("Die Zahl ist eine 0");
+			break;
+			
+		case 3:
+			System.out.println("Die Zahl ist eine 3");
+			break;
+			
+		case 5:
+			System.out.println("Die Zahl ist eine 5");
+			break;
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
