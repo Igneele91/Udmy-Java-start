a1bf1b4c1ba4cacce9d93bbb601caea3cf44955e
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-22 12:23:13 +0200

    tes scanner class
    for test ist extra to understand 1

---
+++ Udemy-Java-Kurs-InputOutput/src/testscani.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class testscani {                   // ist nur Zusatz übung
+
+	public static void main(String[] args) {
+		
+		// Mit der Klasse Scannner hat man Methode zur Verfügung um Eingabe-Strömen (InputStream) einlesen
+		// d.h. Werte einlesen wie z.B. Zahlen
+
+		
+		Scanner scanner = new Scanner(System.in);    // System.in = Input = tastatur
+		
+		
+		
+		// nextInt()     -    Int
+		// nextFloat()   -    Float
+		// nextDouble()  -    Double
+		// nextLong()    -    Long
+		// nextShort()   -    Short
+		// next()        -    Einzelnes Wort
+		// nextLine()    -    String "Zeichenkette"
+		//nextBoolean()  -    Boolean
+		
+		
+	}
+
+}
