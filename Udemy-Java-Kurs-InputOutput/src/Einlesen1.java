b426a0e2bb63b393a333867c5dd57c5437c18c92
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-22 10:09:39 +0200

    scan Funktion
    java scan funktion learning and testing

--- Udemy-Java-Kurs-InputOutput/src/Einlesen1.java
+++ Udemy-Java-Kurs-InputOutput/src/Einlesen1.java
@@ -1,17 +1,17 @@
+import java.util.Scanner;
 
 public class Einlesen1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		// Klasse = Komplexer Datentyp sprich dort sind mehr als nur eine Information gespeichert
 		
-		
-		
-		
-		// hihih
-		
-		
-		// halllo
+	
+		Scanner scan = new Scanner(System.in);           // interaktion mit der Konsole zum sachen eingeben.
+		System.out.println(" Bitte eine Zahl eintippen ...... " );
+		double zahl = scan.nextDouble();
+		System.out.println(" Ihre Zahl lautet: " + zahl );
 		
 		
 		
