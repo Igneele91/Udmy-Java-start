2dc8549134241dbd4287ff04f0c5fd863ccce565
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-22 12:54:49 +0200

    Upload the project if and othes
    is for upload the code i wil change and work again

---
+++ Udemy-Java-Kurs-Kontrollstrukturen/src/AufgabeVerzweigung.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class AufgabeVerzweigung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Kontrollstrukturen steuern den Programmablaufplan 
+		
+		// if = falls
+		// else = ansonsten
+		
+		System.out.println("Wie viele Bundesländer hat Deutschland?");
+		System.out.println("15 oder 3 oder 16 oder 30");
+		
+		Scanner scan = new Scanner(System.in);
+		int eingabeUser = scan.nextInt();
+		
+		if (eingabeUser == 15){
+			System.out.println("Das ist falsch");
+		}else if (eingabeUser == 3){
+			System.out.println("Das ist falsch");
+		}else if (eingabeUser == 16){
+			System.out.println("Das ist richtig !!");
+			System.out.println("Wie heißt der Bundespräsident?");
+		}else if (eingabeUser == 30){
+			System.out.println("Das ist falsch");
+		}
+	}
+
+}
---
+++ Udemy-Java-Kurs-Kontrollstrukturen/src/VerzweigungIfElse.java
@@ -0,0 +1,44 @@
+
+public class VerzweigungIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Kontrollstrukturen steuern den Programmablaufplan 
+		
+				// if = falls
+				// else = ansonsten
+		
+		int i = 10;
+		
+		if (i == 1){
+			System.out.println("Zahl in i ist: " + i);
+		}else if (i >= 1){
+			System.out.println("Zahl in i ist: " + i);
+		}else if (i <= 1){
+			System.out.println("Zahl in i ist: " + i);
+		}else {
+			System.out.println("Zahl unbekannt!");
+		}
+		
+		
+		int score = 8000;
+		int lebenPlayer = 3;
+		
+		if (score == 1000 ){
+			System.out.println("Du hast 1 Stern von 3 Sternen erhalten");
+		}else if (score == 5000){
+			System.out.println("Du hast 2 Sterne von 3 Sternen erhalten");
+		}else if (score > 5000){
+			System.out.println("Du hast 3 von 3 Sternen erhalten!");
+			lebenPlayer += 1;
+			System.out.println("Deine Leben betragen: " + lebenPlayer);
+		}
+		
+		
+		
+		
+		
+	}
+
+}
