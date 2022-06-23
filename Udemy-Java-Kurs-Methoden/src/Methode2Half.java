a23eb13d5db1e68ea128c1cbdfb87c6386aa6dc1
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-23 11:50:14 +0200

    updtae again correct writing

--- Udemy-Java-Kurs-Methoden/src/Methode2Half.java
+++ Udemy-Java-Kurs-Methoden/src/Methode2Half.java
@@ -4,6 +4,29 @@ public class Methode2Half {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Modifikator Rückgabetyp Methodenname ( Datentyp Parameter1, Datentyp
+		// Parameter2 )>
+		// Ein Modifikator gibt den Sichtbereich der Methoden an, es gibt public, privat
+		// und protected
+
+		// Merke: Eine Methode erledigt eine bestimmte Aufgabe
+
+//		for(Anfangswerte; Abbruchbedingungen; Zähler)
+//		for (int i = 1; i<=10; i++) {
+//			erstelleGegner();
+//			// System.out.println();
+//		}
+
+	}
+
+//		public static void erstelleGegner() {
+//			String gegnerName = "Olaf";
+//			int gegnerLeben = 100;
+//			System.out.println("Gegner wurde erstellt");
+//						
+
+	public static void erstelleGegner() {
+
 	}
 
 }
