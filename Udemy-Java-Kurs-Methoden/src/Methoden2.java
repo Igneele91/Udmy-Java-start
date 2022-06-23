5eb75e4f49484f2d0e2b60564c7640044f75f12d
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-23 11:39:18 +0200

    methode 2 und etc

---
+++ Udemy-Java-Kurs-Methoden/src/Methoden2.java
@@ -0,0 +1,45 @@
+import java.security.PublicKey;
+
+public class Methoden2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Modifikator Rückgabetyp Methodenname ( Datentyp Parameter1, Datentyp Parameter2 )>
+		// Ein Modifikator gibt den Sichtbereich der Methoden an, es gibt public, privat und protected
+		
+		// Merke: Eine Methode erledigt eine bestimmte Aufgabe
+	
+		for (int i = 1; i<=10; i++) {
+			erstelleGegner();
+			// System.out.println();
+		}
+		
+	
+		
+						
+	}
+
+	
+	
+	public static void erstelleGegner() {
+		String gegnerName = "Olaf";
+		int gegnerLeben = 100;
+		System.out.println("Gegner wurde erstellt");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
