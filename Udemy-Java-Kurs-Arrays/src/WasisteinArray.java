77aaa9da06c59c19865a002135e71c0e6a6920f6
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-23 15:23:09 +0200

    new array part java interess time is runnig

---
+++ Udemy-Java-Kurs-Arrays/src/WasisteinArray.java
@@ -0,0 +1,21 @@
+
+public class WasisteinArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Variable Dekleration
+		// Datentyp Name = Wert;
+		
+		
+		// Array Deklaration
+		// Datentyp [] Name = new Datentyp[Anzahl];
+		
+		
+		
+		
+		
+		
+	}
+
+}
