3ec6176696f279b498b670e2eeada39ce4a336ad
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-23 11:09:45 +0200

    uploadee methode 1 first try

---
+++ Udemy-Java-Kurs-Methoden/src/Methoden.java
@@ -0,0 +1,41 @@
+import java.security.PublicKey;
+
+public class Methoden {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ * Modifikator Rückgabetyp Methodenname ( Datentyp Parameter1, Datentyp Parameter2 )>
+ * Ein Modifikator gibt den Sichtbereich der Methoden an, es gibt public, privat und protected
+ */
+		// void = leer
+		
+		ausgabe();
+		summe();
+	}
+	
+	
+	public  static void ausgabe() {
+		System.out.println( "Ich bin eine Methode ");
+			
+		}
+		
+		
+		public static void summe() {
+			
+			int a,b, summe;
+			
+			a = 5;
+			b = 5;
+			
+			summe = a + b;
+			
+			System.out.println(summe);
+		}
+		
+		
+		
+		
+	
+
+}
